17288a52622500918d4c52948d728b478faa2da9
Nisha <nishasoni0011@gmail.com>
2024-01-28 22:14:35 -0500

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+export const lusitana = {
+    fontFamily: 'Lusitana, serif',
+    fontWeight: 'bold',
+    // Add other font styles as needed
+    className: 'font-bold', // You can add additional classes here
+  };
+
+export const inter = Inter({ subsets: ['latin'] });
